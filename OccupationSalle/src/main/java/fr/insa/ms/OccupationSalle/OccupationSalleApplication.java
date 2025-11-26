1a412928f872399c401cba01307a5d23b7c60bb9
Lassalle Tom <tlassalle@insa-20390>
2025-11-26 16:29:39 +0100

    Fichier projet

---
+++ OccupationSalle/src/main/java/fr/insa/ms/OccupationSalle/OccupationSalleApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.OccupationSalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupationSalleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupationSalleApplication.class, args);
+	}
+
+}
---
+++ OccupationSalle/src/test/java/fr/insa/ms/OccupationSalle/OccupationSalleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.OccupationSalle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupationSalleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
