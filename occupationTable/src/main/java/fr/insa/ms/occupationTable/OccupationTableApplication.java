3d55ac8bc4ae1b96d7ede33832f840c6d3981a6b
Lassalle Tom <tlassalle@modele-ltsp-noble>
2025-12-03 16:30:52 +0100

    Ajout occupation table

---
+++ occupationTable/src/main/java/fr/insa/ms/occupationTable/OccupationTableApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.occupationTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OccupationTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OccupationTableApplication.class, args);
+	}
+
+}
---
+++ occupationTable/src/test/java/fr/insa/ms/occupationTable/OccupationTableApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.occupationTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OccupationTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
