3337ed73632e2abf7685c0e818960c5ff250b51d
Lassalle Tom <tlassalle@modele-ltsp-noble>
2025-12-03 16:28:10 +0100

    Ajout gestion Table

---
+++ gestionTable/src/test/java/fr/insa/ms/gestionTable/GestionTableApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.insa.ms.gestionTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
